8935695555d5079d8ef67ed1d2f7347f44d151ae
rootfellen <rootfellenua@gmail.com>
2023-08-13 19:06:51 -0700

    add store navbar

---
+++ store/components/navbar-actions.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+import Button from "@/components/ui/button";
+
+const NavbarActions = () => {
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button></Button>
+    </div>
+  );
+};
+
+export default NavbarActions;
