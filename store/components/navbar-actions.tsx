48bb11f18c17c9d933701db9f8a1087ad2530122
rootfellen <rootfellenua@gmail.com>
2023-08-18 12:56:19 -0700

    add functionality add to cart

--- store/components/navbar-actions.tsx
+++ store/components/navbar-actions.tsx
@@ -3,6 +3,8 @@
 import React, { useState, useEffect } from "react";
 import Button from "@/components/ui/button";
 import { ShoppingBag } from "../node_modules/lucide-react";
+import useCart from "@/hooks/use-cart";
+import { useRouter } from "next/navigation";
 
 const NavbarActions = () => {
   // prevent hydration errors as we are going to store number of cart items in the localsStorage
@@ -13,14 +15,22 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
+  const cart = useCart();
+
   if (!isMounted) {
     return null;
   }
   return (
     <div className="ml-auto flex items-center gap-x-4">
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button
+        onClick={() => router.push("/cart")}
+        className="flex items-center rounded-full bg-black px-4 py-2"
+      >
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm font-medium text-white">0</span>
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
